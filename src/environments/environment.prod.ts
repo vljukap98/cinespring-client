a76b6dc85048e76991029895d1d1b6d335c71866
Luka Jaković <30662155+vljukap98@users.noreply.github.com>
2022-09-07 13:48:25 +0200

    add prod conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  authApiUrl: 'https://cinespring-postgresql.herokuapp.com/auth',
+  movieApiUrl: 'https://cinespring-postgresql.herokuapp.com/movie',
+  favoriteApiUrl: 'https://cinespring-postgresql.herokuapp.com/favorites',
+  watchedApiUrl: 'https://cinespring-postgresql.herokuapp.com/watched',
+  watchlistApiUrl: 'https://cinespring-postgresql.herokuapp.com/watchlist',
+  searchApiUrl: 'https://cinespring-postgresql.herokuapp.com/search',
 };
