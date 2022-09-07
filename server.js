f99e798ecca44012df1b22134cacc7a278a3ea9e
Luka Jaković <30662155+vljukap98@users.noreply.github.com>
2022-09-07 15:06:07 +0200

    #

---
+++ server.js
@@ -0,0 +1,14 @@
+//Install express server    
+const express = require('express');
+const path = require('path');   
+const app = express();   
+
+// Serve only the static files form the dist directory    
+app.use(express.static('./dist/cinespring-client'));
+
+app.get('/*', (req,res) => {  
+    res.sendFile('index.html', {root: 'dist/cinespring-client'});   
+});  
+
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
