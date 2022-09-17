abb1917c599edd2214812df5e7ece96f045ec269
Luka Jaković <30662155+vljukap98@users.noreply.github.com>
2022-09-17 20:21:02 +0200

    logout user if token expiers in the meanwhile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './auth/auth.service';
 
 @Component({
@@ -6,14 +7,28 @@ import { AuthService } from './auth/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cinespring-client';
+  isLoggedIn: boolean = false;
 
-  constructor(private authService: AuthService) {
-    const now = new Date().getTime();
+  constructor(private authService: AuthService, private ngZone: NgZone, private router: Router) {  }
 
-    if(now > this.authService.getExpires()) {
-      this.authService.logout();
-    }
+  ngOnInit(): void {
+    this.authService.loggedIn.subscribe((data) => this.isLoggedIn = data)
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.checkIfTokenExpired();
+  }
+
+  checkIfTokenExpired() {
+    this.ngZone.runOutsideAngular(() => {
+      setInterval(() => {
+        const now = new Date().getTime();
+
+        if(this.isLoggedIn && (now > this.authService.getExpires())) {
+          this.authService.logout();
+          this.router.navigateByUrl('/login');
+        }
+      }, 1);
+    });
   }
 }
